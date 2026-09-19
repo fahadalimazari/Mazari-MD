f9a4b9820853bd9732b6e29db3ce61f95a7eac89
Fahad Mazari <fahadmazari@example.com>
2026-09-19 11:49:30 +0500

    Change yts command category to download

--- plugins/yts.js
+++ plugins/yts.js
@@ -6,7 +6,7 @@ cmd({
     alias: ["ytsearch"],
     react: "☺️",
     desc: "Search videos on YouTube",
-    category: "search",
+    category: "download",
     use: ".yts <video name>",
     filename: __filename
 },
